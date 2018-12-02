6088a0ed8975812b312d6da4e16541fda815f944
pradeeppeary <44652284+pradeeppeary@users.noreply.github.com>
2018-12-02 23:32:23 +0530

    Update Magnet.java

--- SnakevsBlock/Magnet.java
+++ SnakevsBlock/Magnet.java
@@ -3,8 +3,18 @@ import java.util.Random;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
-
+/**
+ * Below is the Magnet class, which extends Token class.
+ * Magnet is type of token. which helps the player to collect more balls.
+ * Which increases the length of the snake and its life.
+ * @author pradeep
+ *
+ */
 public class Magnet extends Token{
+	/**
+	 * Below is the non-Parameterized creates the magnet randomly.
+	 * It sets the coordinate of the magnet randomly.
+	 */
 	public Magnet(){
 		super(new Circle());
 		Image magnet=new Image("file:12.png");
