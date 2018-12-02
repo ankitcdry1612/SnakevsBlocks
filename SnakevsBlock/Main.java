997033a6fd16131bcfb69b3c834534f0c32d6e3b
pradeeppeary <44652284+pradeeppeary@users.noreply.github.com>
2018-12-02 23:33:15 +0530

    Update Main.java

--- SnakevsBlock/Main.java
+++ SnakevsBlock/Main.java
@@ -7,15 +7,29 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
+/**
+ * This is the main class which executes the game.
+ * This class extends the application class.
+ * @author pradeep
+ *
+ */
 public class Main extends Application {
 	static Leaderboard board;
 	static ObjectInputStream in=null;
 	static ObjectOutputStream out=null;
+	/**
+	 * This is the main method which launches the game.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
 	}
+	/**
+	 * This is the method in the application class which is used to start the game.
+	 * And creates a stage for the game.
+	 * This method also throws the exceptions.
+	 */
 
 	@Override
 	public void start(Stage stage) throws Exception {
