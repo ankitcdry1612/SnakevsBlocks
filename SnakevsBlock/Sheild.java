d73563c070afa7a12cd71550443639937c28de6a
pradeeppeary <44652284+pradeeppeary@users.noreply.github.com>
2018-12-02 23:42:15 +0530

    Update Sheild.java

--- SnakevsBlock/Sheild.java
+++ SnakevsBlock/Sheild.java
@@ -3,8 +3,20 @@ import java.util.*;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
-
+/**
+ * Below is the shield class.
+ * The shield is used to protect the snake, from all the blocks.
+ * @author pradeep
+ * with the help of shield, snake is not going to die for a specific period.
+ * Irrespective of the fact, it collides with the blocks of greater value.
+ * The sheild class extends token class.
+ */
 public class Sheild extends Token {
+	/**
+	 * Below is the non-parameterized constructor of the shield class.
+	 * It sets the coordinate of the shield, which are randomly generated.
+	 * Also sets the image over the shield.
+	 */
 	public Sheild() {
 		super(new Circle());
 		Image shield = new Image("file:shield.jpeg");
